fa97c576b2108fa63fa5edbc22b33f117c2ea8dd
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-01-10 00:37:42 +0900

    `NaverService` 추가

---
+++ src/app/naver/service.ts
@@ -0,0 +1,32 @@
+import Module from "./module";
+
+export default class NaverService {
+  private readonly module: Module;
+
+  constructor(module: Module) {
+    this.module = module;
+  }
+
+  async login(id: string, password: string) {
+    await this.module.urlChanger.moveToLoginURL();
+    await this.module.pageInteractor.login(id, password);
+  }
+
+  async getHistory() {
+    const history = [];
+    await this.module.urlChanger.moveToPaymentHistoryURL();
+    await this.module.pageInteractor.loadPaymentHistoryUntilPageEnds();
+
+    const paymentElements =
+      await this.module.elementParser.parsePaymentElements();
+    for (const element of paymentElements) {
+      const payment = await this.module.elementParser.parseElement(element);
+      if (!payment) {
+        continue;
+      }
+      history.push(payment);
+    }
+
+    return history;
+  }
+}
