64d3b5a4d303d9daa5e34fce2b19c2cc86621a5c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-01-10 16:51:18 +0900

    login 과정에 `Promise.all` 을 사용하도록 변경

--- src/app/naver/pageInteractor.ts
+++ src/app/naver/pageInteractor.ts
@@ -9,16 +9,21 @@ export default class PageInteractor {
   }
 
   async login(id: string, password: string, delay?: number) {
-    await this.page.waitForSelector("#id");
-    await this.page.waitForSelector("#pw");
+    await Promise.all([
+      this.page.waitForSelector("#id"),
+      this.page.waitForSelector("#pw"),
+    ]);
 
     await this.page.focus("#id");
     await this.page.keyboard.type(id, { delay: delay || 200 });
     await this.page.focus("#pw");
     await this.page.keyboard.type(password, { delay: delay || 200 });
-    await this.page.click("#log\\.login");
 
-    await this.page.waitForNavigation({ waitUntil: "domcontentloaded" });
+    await Promise.all([
+      this.page.click("#log\\.login"),
+      this.page.waitForNavigation({ waitUntil: "networkidle2" }),
+    ]);
+
     if (this.page.url().includes("https://nid.naver.com/nidlogin.login")) {
       throw new Error("Login failed");
     }
