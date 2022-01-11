4734dac82ffbb1e168a1e4be03466d823aa403d7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-01-11 12:26:30 +0900

    사용하지 않는 `types` 제거

--- src/types/payment.d.ts
+++
@@ -1,9 +0,0 @@
-import PAYMENT_HISTORY_STATUS from "./paymentHistoryStatus";
-
-export default interface Payment {
-  readonly thumbnailUrl?: string;
-  readonly title: string;
-  readonly price: number;
-  readonly status: PAYMENT_HISTORY_STATUS;
-  readonly purchasedAtTimestamp?: number;
-}
