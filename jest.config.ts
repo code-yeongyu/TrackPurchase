7e8f5cceef8784473b721319678d5eacd5307984
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-01-11 23:31:42 +0900

    jest 설정 package.json 으로 이동

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "@jest/types";
-
-// Sync object
-const config: Config.InitialOptions = {
-  preset: "jest-puppeteer",
-  coveragePathIgnorePatterns: ["elementParser.ts"],
-};
-export default config;
