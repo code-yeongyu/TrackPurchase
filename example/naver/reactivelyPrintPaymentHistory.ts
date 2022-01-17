c1481a3c87df9b261a7e8267b58a3ae6dd3c5b45
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-01-17 18:55:36 +0900

    예시 코드 업데이트

--- example/naver/reactivelyPrintPaymentHistory.ts
+++ example/naver/reactivelyPrintPaymentHistory.ts
@@ -2,15 +2,17 @@ import puppeteer from "puppeteer";
 import { NaverApp } from "trackpurchase";
 
 import readline from "readline";
+import { concat, defer, filter, from, tap } from "rxjs";
 
 const printNaverPayHistory = async (id: string, password: string) => {
   const MOBILE_UA =
     "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1";
 
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: ["--start-maximized"],
   });
+
   const page = await browser.newPage();
   await page.setViewport({ height: 800, width: 1200 });
   await page.setUserAgent(MOBILE_UA);
@@ -18,10 +20,29 @@ const printNaverPayHistory = async (id: string, password: string) => {
   const module = NaverApp.ModuleFactory.create(page);
   const crawlService = new NaverApp.Service(module);
 
-  await crawlService.normalLogin(id, password);
+  const loginEvent$ = crawlService.interactiveLogin(id, password);
+  const history$ = defer(() => from(crawlService.getHistory()));
+  const closePage$ = defer(() => from(page.close()));
+  const closeBrowser$ = defer(() => from(browser.close()));
 
-  const history = await crawlService.getHistory();
-  console.log(history);
+  const final$ = concat(loginEvent$, history$, closePage$, closeBrowser$).pipe(
+    tap((event) => {
+      if (event === "otp-required") {
+        console.log("스마트폰 앱에서 OTP 인증을 완료해주세요.");
+      }
+    }),
+    tap((event) => {
+      if (event === "manual-otp-required") {
+        rl.question("otp code: ", async (code) => {
+          module.pageInteractor.fillManualOTPInput(code);
+        });
+      }
+    }),
+    filter((event) => event instanceof Array)
+  );
+  final$.subscribe((event) => {
+    console.log(event);
+  });
 };
 
 const rl = readline.createInterface({
