893400e1627d789eafce3673328c6234b112ad49
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-02-10 00:24:36 +0900

    `nextPaymentHistory` 추가

--- src/app/naver/scraper.test.ts
+++ src/app/naver/scraper.test.ts
@@ -24,4 +24,37 @@ describe("Scraper", () => {
       );
     });
   });
+
+  describe("nextPaymentHistory", () => {
+    it("should create an post request", async () => {
+      // given
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const cookie = "";
+      const scraper = new NaverScraper();
+      const postSpy = jest.spyOn(axios, "post");
+      postSpy.mockImplementation(() => Promise.resolve({ data: {} }));
+
+      // when
+      await scraper.nextPaymentHistory(
+        cookie,
+        "order-1234",
+        new Date().getTime() - day * 30
+      );
+
+      // then
+      expect(postSpy).toBeCalledWith(
+        scraper.nextPaymentHistoryURL,
+        expect.any(Object),
+        expect.objectContaining({
+          headers: expect.objectContaining({
+            Cookie: cookie,
+          }),
+        })
+      );
+    });
+  });
 });
--- src/app/naver/scraper.ts
+++ src/app/naver/scraper.ts
@@ -1,5 +1,7 @@
 import { CommonResponse } from "app/common/types/response";
 import axios from "axios";
+import { ServiceGroup } from "./types/serviceGroup";
+import { StatusGroup } from "./types/statusGroup";
 
 export class NaverScraper {
   private async getTodayString() {
@@ -29,6 +31,43 @@ export class NaverScraper {
 
     const response = await axios.post(
       this.searchPaymentHistoryURL,
+      data,
+      options
+    );
+
+    return { status: response.status, data: response.data as string };
+  }
+
+  nextPaymentHistoryURL =
+    "https://new-m.pay.naver.com/api/timeline/nextPaymentHistory";
+  async nextPaymentHistory(
+    cookie: string,
+    lastHistoryId: string,
+    lastHistoryDateTimestamp: number,
+    searchOptions?: {
+      keyword: string;
+      serviceGroup: ServiceGroup;
+      statusGroup: StatusGroup;
+    }
+  ): Promise<CommonResponse> {
+    const data = {
+      keyword: searchOptions?.keyword || null,
+      startDate: "2000-01-01",
+      endDate: await this.getTodayString(),
+      serviceGroup: searchOptions?.serviceGroup || null,
+      statusGroup: searchOptions?.statusGroup || null,
+      lastId: lastHistoryId,
+      lastDate: lastHistoryDateTimestamp,
+    };
+    const options = {
+      headers: {
+        Cookie: cookie,
+        "content-type": "application/json;charset=UTF-8",
+      },
+    };
+
+    const response = await axios.post(
+      this.nextPaymentHistoryURL,
       data,
       options
     );
