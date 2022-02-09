7c7fef1cb8753a4dcd8d5e169164fbb8a3ed6be6
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-02-10 02:55:58 +0900

    `parseInformationForNextPaymentHistory` 추가

--- src/app/naver/parser.spec.ts
+++ src/app/naver/parser.spec.ts
@@ -17,4 +17,21 @@ describe("NaverParser", () => {
       expect(result).toEqual(expectedPaymentHistoryItems);
     });
   });
+
+  describe("parseInformationForNextPaymentHistory", () => {
+    it("should parse", () => {
+      // given
+      const parser = new NaverParser();
+
+      // when
+      const result = parser.parseInformationForNextPaymentHistory(
+        searchPaymentHistoryJson
+      );
+
+      // then
+      expect(result.hasNext).toBe(true);
+      expect(result.lastHistoryId).toBe("order-2021110243152861");
+      expect(result.lastHistoryDateTimestamp).toBe(1635853254000);
+    });
+  });
 });
--- src/app/naver/parser.ts
+++ src/app/naver/parser.ts
@@ -24,4 +24,17 @@ export class NaverParser {
     });
     return paymentHistories;
   }
+
+  parseInformationForNextPaymentHistory(jsonString: string): {
+    hasNext: boolean;
+    lastHistoryId: string;
+    lastHistoryDateTimestamp: number;
+  } {
+    const data = JSON.parse(jsonString) as PaymentHistoryResponse;
+
+    const hasNext = data.result.hasNext;
+    const lastHistoryId = data.result.lastId;
+    const lastHistoryDateTimestamp = data.result.lastDate;
+    return { hasNext, lastHistoryId, lastHistoryDateTimestamp };
+  }
 }
