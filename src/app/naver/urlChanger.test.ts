876be5457e54bccb9b4a34c5a5e709f70ea14d9c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-02-10 16:40:24 +0900

    사용하지 않는 메소드 `moveToPaymentHistoryURL` 제거

--- src/app/naver/urlChanger.test.ts
+++ src/app/naver/urlChanger.test.ts
@@ -1,31 +1,17 @@
 import URLChanger from "./urlChanger";
 
-describe("Login", () => {
-  it("Should move page to login", async () => {
-    // given
-    const urlChanger = new URLChanger(page);
-    const pageSpy = jest.spyOn(page, "goto");
+describe("URLChanger", () => {
+  describe("moveToLoginURL", () => {
+    it("Should move page to login", async () => {
+      // given
+      const urlChanger = new URLChanger(page);
+      const pageSpy = jest.spyOn(page, "goto");
 
-    // when
-    await urlChanger.moveToLoginURL();
+      // when
+      await urlChanger.moveToLoginURL();
 
-    // then
-    expect(pageSpy).toHaveBeenCalledWith(urlChanger.loginURL);
-  });
-});
-
-describe("PaymentHistory", () => {
-  it("Should move page to payment history", async () => {
-    // given
-    const urlChanger = new URLChanger(page);
-    const pageSpy = jest.spyOn(page, "goto");
-    const waitForSelectorSpy = jest.spyOn(page, "waitForSelector");
-    waitForSelectorSpy.mockImplementation(() => Promise.resolve(null));
-
-    // when
-    await urlChanger.moveToPaymentHistoryURL();
-
-    // then
-    expect(pageSpy).toHaveBeenCalledWith(urlChanger.paymentHistoryURL);
+      // then
+      expect(pageSpy).toHaveBeenCalledWith(urlChanger.loginURL);
+    });
   });
 });
